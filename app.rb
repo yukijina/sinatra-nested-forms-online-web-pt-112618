1bea9b0f71d1aa80315ce1f3d1b1f344be7805e9
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-03-14 23:15:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,19 +8,19 @@ module FormsLab
     end
 
     get '/new' do
+
       erb :"pirates/new"
     end
 
     post '/pirates' do
-      binding.pry
       @name = params[:pirate][:name]
       @weight = params[:pirate][:weight]
       @height = params[:pirate][:height]
       @ships = params[:pirate][:ships]
       Pirate.new(@name, @weight, @height)
+    
       erb :"pirates/show"
     end
-
-
+    
   end
 end
