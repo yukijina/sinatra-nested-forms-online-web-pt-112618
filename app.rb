7ca0b6e606c3327b3abff40beaf32f2840a6f373
yukijina <yukijina@gmail.com>
2019-03-15 07:11:55 -0700

    Done.

--- app.rb
+++ app.rb
@@ -12,12 +12,22 @@ module FormsLab
     end
 
     post '/pirates' do
-      binding.pry
+
       @name = params[:pirate][:name]
       @weight = params[:pirate][:weight]
       @height = params[:pirate][:height]
       @ships = params[:pirate][:ships]
       Pirate.new(@name, @weight, @height)
+
+      @ships.each do |ship|
+        new_ship = Ship.new.tap do |s|
+          s.name = ship[:name]
+          s.type = ship[:type]
+          s.booty = ship[:booty]
+        end
+      end
+     @all_ships = Ship.all
+
       erb :"pirates/show"
     end
 
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,17 +1,15 @@
 class Ship
-  attr_reader :name, :type, :booty
+  attr_accessor :name, :type, :booty
   @@all = []
 
-  def initialize(name,type,booty)
-      @name = name
-      @type = type
-      @booty = booty
+  def initialize(name: name ,type: type, booty: booty)
+      @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
   def self.clear
     @@all.clear
   end
